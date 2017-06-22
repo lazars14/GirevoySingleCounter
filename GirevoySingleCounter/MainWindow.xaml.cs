c6a7897186ea7c65f70447dd9be4c8a99fbe6ac7
zomlaaa95 <zomlaaa95@gmail.com>
2017-06-22 12:17:34 +0200

    Created layout for main window

--- GirevoySingleCounter/MainWindow.xaml.cs
+++ GirevoySingleCounter/MainWindow.xaml.cs
@@ -20,9 +20,29 @@ namespace GirevoySingleCounter
     /// </summary>
     public partial class MainWindow : Window
     {
+        private int minutes { get; set; }
+        private static int DEFAULT_MINUTES_VALUE = 30;
+
         public MainWindow()
         {
             InitializeComponent();
+            minutes = 0;
+        }
+
+        private void new_interval_btn_Click(object sender, RoutedEventArgs e)
+        {
+            int i = 0;
+            string minutes_text = new_interval_txtb.Text;
+            if (!int.TryParse(minutes_text, out i))
+            {
+                minutes = DEFAULT_MINUTES_VALUE;
+            }
+            else
+            {
+                minutes = i;
+            }
+
+
         }
     }
 }

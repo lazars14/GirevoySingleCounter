7bc2f792620cbbce6a28bd7f6c83af32c3b28633
zomlaaa95 <zomlaaa95@gmail.com>
2017-06-22 13:47:10 +0200

    Implemented logic for key press events

--- GirevoySingleCounter/CounterWindow.xaml.cs
+++ GirevoySingleCounter/CounterWindow.xaml.cs
@@ -1,16 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace GirevoySingleCounter
 {
@@ -19,11 +10,45 @@ namespace GirevoySingleCounter
     /// </summary>
     public partial class CounterWindow : Window
     {
+        private MainWindow main_window { get; set; }
+        private DispatcherTimer timer { get; set; }
+
         private int counter { get; set; }
 
-        public CounterWindow()
+        public CounterWindow(MainWindow mainWindow, int minutes)
         {
             InitializeComponent();
+            counter = 0;
+
+            main_window = mainWindow;
+            setDispatchTimer(minutes);
+            this.KeyDown += HandleKeyPress;
+        }
+
+        private void setDispatchTimer(int minutes)
+        {
+            timer = new DispatcherTimer();
+            timer.Interval = new TimeSpan(0, minutes, 0);
+        }
+
+        private void HandleKeyPress(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.Return:
+                    timer.Start();
+                    break;
+                case Key.Space:
+                    ++counter;
+                    break;
+                case Key.Subtract:
+                    --counter;
+                    break;
+                case Key.Escape:
+                    main_window.Show();
+                    this.Close();
+                    break;
+            }
         }
     }
 }
